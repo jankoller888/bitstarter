6186cb72aa1d4176d08f702c438e78c8042d922e
jankoller888 <jankoller888@gmail.com>
2013-07-10 09:03:22 -0700

    read str form index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
+var buff = new Buffer(27);
+var fd = fs.openSync('index.html','r');
+fs.readSync(fd,buff,0,buff.length,0);
+fs.closeSync(fd);
+//console.log(buff.toString());
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
