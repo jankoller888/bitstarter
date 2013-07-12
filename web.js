9d7559bd447c6552c109fccb35998b40609359f4
jankoller888 <jankoller888@gmail.com>
2013-07-12 07:55:51 -0700

    fixed web.js

--- web.js
+++ web.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var fs = require('fs');
-var buff = new Buffer(27);
-var fd = fs.openSync('index.html','r');
-fs.readSync(fd,buff,0,buff.length,0);
-fs.closeSync(fd);
-//console.log(buff.toString());
+var buff = fs.readFileSync('index.html');
+console.log(buff.toString());
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
