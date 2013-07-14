761f1049f5181ccd2b27339e96bf6fceebca2533
jankoller888 <jankoller888@gmail.com>
2013-07-14 00:37:20 -0700

    fixed bugs with async in request URL

--- grader.js
+++ grader.js
@@ -27,7 +27,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "index.html";
+//var URL_DEFAULT = "index.html";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -36,18 +36,6 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
-var requestUrl = function(urlreq){
-	var inst
-	rest.get(urlreq).on('complete', function(result) {
-	if (result instanceof Error) {
-    sys.puts('Error: ' + result.message);
-    this.retry(5000); // try again after 5 sec
-  } else {
-     //fs.writeFileSync('request.html', result);
-	 this.inst = result
-  }
-  return inst;
-})};
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -68,6 +56,24 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var CheckUrl = function(urlreq,checksfile){
+	rest.get(urlreq).on('complete', function(result) {
+	if (result instanceof Error) {
+    console.log('Error: ' + result.message);
+	process.exit(1);
+  } else {
+     $=cheerio.load(result);
+	 var checks = loadChecks(checksfile).sort();
+	 var out = {};
+     for(var ii in checks) {
+		 var present = $(checks[ii]).length > 0;
+         out[checks[ii]] = present;
+    }
+	 console.log(JSON.stringify(out, null, 4));
+  }
+  
+})};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -77,12 +83,18 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-		.option('-u, --url <url_link>', 'Url link to a page', clone(requestUrl), URL_DEFAULT)
+		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url_link>', 'Url link to a page')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+		//console.log("url link:"+program.url);
+	if (program.url !== undefined){
+		CheckUrl(program.url, program.checks);
+	}
+	else{
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+		}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
